f8fef8343ed6dbc009e5b1d265ff96c2897b7ffb
morozov <morozov82@mail.ru>
2022-04-22 21:29:51 +0300

    добавлена проверка схемы XSD

--- src/test/java/ru/demo/DemoApplicationTests.java
+++ src/test/java/ru/demo/DemoApplicationTests.java
@@ -5,15 +5,22 @@ import generated.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.StreamUtils;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.UnmarshalException;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -37,6 +44,8 @@ class DemoApplicationTests {
     public void parseXmlUseJackson() throws IOException {
         String xml = StreamUtils.copyToString(DemoApplicationTests.class.getClassLoader().getResourceAsStream("test.xml"), StandardCharsets.UTF_8);
 
+        //without validation
+
         Main main = objectMapper.readValue(xml, Main.class);
 
         System.out.println(new ObjectMapper().writeValueAsString(main));
@@ -46,17 +55,40 @@ class DemoApplicationTests {
     }
 
     @Test
-    public void parseXmlUseJaxb() throws IOException, JAXBException {
+    public void parseXmlUseJaxb() throws IOException, JAXBException, SAXException {
         String xml = StreamUtils.copyToString(DemoApplicationTests.class.getClassLoader().getResourceAsStream("test.xml"), StandardCharsets.UTF_8);
 
         JAXBContext context = JAXBContext.newInstance(Main.class);
         javax.xml.bind.Unmarshaller um = context.createUnmarshaller();
+
+        //without validation
+
         Main main = (Main) um.unmarshal(new StringReader(xml));
 
         System.out.println(new ObjectMapper().writeValueAsString(main));
 
         assertThat(main.getRoleType().getRoleId()).isNotEmpty();
         assertThat(main.getRoleType().getRoleId()).containsExactlyInAnyOrder("1", "3");
+    }
+
+    @Test
+    public void parseXmlUseJaxbWithValidate() throws IOException, JAXBException, SAXException {
+        String xml = StreamUtils.copyToString(DemoApplicationTests.class.getClassLoader().getResourceAsStream("test.xml"), StandardCharsets.UTF_8);
+
+        JAXBContext context = JAXBContext.newInstance(Main.class);
+        javax.xml.bind.Unmarshaller um = context.createUnmarshaller();
+
+        //set schema for validation
+        SchemaFactory sf = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = sf.newSchema(ClassUtils.getDefaultClassLoader().getResource("xsd/test.xsd"));
+        um.setSchema(schema);
+
+        Exception exception = assertThrows(UnmarshalException.class, () -> {
+            um.unmarshal(new StringReader(xml));
+        });
+
+        exception.printStackTrace();
+        assertThat(exception.toString()).contains("is expected");
     }
 
 }
